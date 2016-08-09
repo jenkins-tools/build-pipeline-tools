33e66e36919c187d01cd6dd8bc99d48ed7d85583
Sunjoo Park <allessunjoo.park@lge.com>
2016-08-09 13:47:26 +0900

    Rename variables

--- Build.groovy
+++ Build.groovy
@@ -21,13 +21,13 @@ node('master_pipeline') {
    def compare_result = readFile 'compare_result'
    if ( compare_result == "CHANGED\n" ) {
        echo "FOSS: " + compare_result
-       stage 'Call a clean engineering build'
+       stage 'Call starfishbdk-official-all'
        echo 'Clean Build'
        def target_job_name = "starfishbdk-official-all"
        def build_machines = machine_name
        currentBuild.description = "Job name    : " + job_name
        currentBuild.description = currentBuild.description + "<br/>" + "Build number: " + build_number 
-       join = parallel([clean: {
+       join = parallel([bdk_build: {
                 build job:target_job_name, parameters: [
                     [$class: 'StringParameterValue',  name:'SDK_BUILD_BRANCH',        value:"@" + branch_name],
                     [$class: 'StringParameterValue',  name:'SDK_BUILD_NUMBER',        value:build_number],
@@ -40,13 +40,13 @@ node('master_pipeline') {
         }
         ])
 
-        def clean_build_result = join.clean.result
-        def clean_build_number = join.clean.number.toString()
+        def bdk_build_result = join.bdk_build.result
+        def bdk_build_number = join.bdk_build.number.toString()
 
-        stage 'Copy bdk result'
-        if (clean_build_result == "SUCCESS" ) {
-            def target_web = web_root +  'starfish/' + target_job_name + '/' + clean_build_number;
-            currentBuild.description += '<br/><a href=\"' + target_web + '\">' + target_job_name + ':' + clean_build_number + '</a>'
+        stage 'Set description'
+        if (bdk_build_result == "SUCCESS" ) {
+            def target_web = web_root +  'starfish/' + target_job_name + '/' + bdk_build_number;
+            currentBuild.description += '<br/><a href=\"' + target_web + '\">' + target_job_name + ':' + bdk_build_number+ '</a>'
         }
     }else {
         currentBuild.description = "No change"
